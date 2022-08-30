28475ad555adbe29f8b041168891c8d63d8e3def
garridomfg <garridomfg@gmail.com>
2022-08-30 09:33:15 +0200

    update api endpoints, create new endpoint

--- src/api/searchApi.ts
+++ src/api/searchApi.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-("/Rom.json?proximity=12.568810991231572%2C42.291896875460054&types=place%2Cpostcode%2Caddress&language=en&access_token=YOUR_MAPBOX_ACCESS_TOKEN");
-
 const searchApi = axios.create({
   baseURL: "https://api.mapbox.com/geocoding/v5/mapbox.places",
   params: {
